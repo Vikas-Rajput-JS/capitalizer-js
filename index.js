9a611ebad9cc5bb0314ea849e745d5b280d571e9
davidthakur <vr384695@gmail.com>
2024-02-04 18:11:38 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const capitalizeFirstLetter =  (str)=> {
+function capitalizeFirstLetter (str) {
     if (!str) return '';
     return str.charAt(0).toUpperCase() + str.slice(1);
+    
   }
-const log = capitalizeFirstLetter('hello developers')
-console.log(log)
 
-module.export = capitalizeFirstLetter;
+module.exports = capitalizeFirstLetter;
