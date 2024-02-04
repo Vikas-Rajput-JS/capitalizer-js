8900ba494c9ece44de7ce0e36cd98247e9a7b3a4
davidthakur <vr384695@gmail.com>
2024-02-04 18:02:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function capitalizeFirstLetter (str){
+const capitalizeFirstLetter =  (str)=> {
     if (!str) return '';
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
-
-
+const log = capitalizeFirstLetter('hello developers')
+console.log(log)
 
 module.export = capitalizeFirstLetter;
